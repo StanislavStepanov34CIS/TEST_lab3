bbc66adf499f8193c462b29776cf1e8810269e26
DESKTOP-ASUS\STAS <mrstasvs@gmail.com>
2022-03-02 13:30:03 +0300

    Stepanov 3

--- Battery.java
+++ Battery.java
@@ -2,7 +2,7 @@ package brokenrobotgame.model;
 
 import brokenrobotgame.model.navigation.CellPosition;
 
-public class Battery extends AbstractBattery, Unit
+public class Battery extends AbstractBattery, Unit, Cell
 {
     public Battery(GameField field, int capacity, int amount)
     {
